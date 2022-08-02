ee587b0bde4ff6c11d287b7e7e07fffbfeb24108
porec <32462260+porec@users.noreply.github.com>
2022-08-02 21:52:34 +0200

    Update lambda.tf

--- terraform/aws/lambda.tf
+++ terraform/aws/lambda.tf
@@ -1,9 +1,32 @@
-resource "aws_lambda_function" "example-lambda" {
-  filename         = "lambda_function.zip"
-  function_name    = "test_lambda"
-  role             = "${aws_iam_role.iam_for_lambda_tf.arn}"
-  handler          = "index.handler"
-  source_code_hash = "${data.archive_file.lambda_zip.output_base64sha256}"
-  runtime          = "nodejs6.10"
-  layers = "arn:aws:lambda:eu-central-1:017000801446:layer:AWSLambdaPowertoolsPython:18"
+resource "aws_iam_role" "iam_for_lambda" {
+  name = "iam_for_lambda"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_lambda_function" "test_lambda" {
+  filename      = "lambda_function_payload.zip"
+  function_name = "lambda_function_name"
+  role          = aws_iam_role.iam_for_lambda.arn
+  handler       = "index.test"
+  runtime       = "nodejs14.x"
+  layers        = "arn:aws:lambda:eu-central-1:017000801446:layer:AWSLambdaPowertoolsPython:18"
+}
+  ephemeral_storage {
+    size = 10240 # Min 512 MB and the Max 10240 MB
+  }
 }
